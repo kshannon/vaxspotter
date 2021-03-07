fb9841dfea8fd656673c85d92279af978d56f284
Kyle Shannon <kyle@kmshannon.com>
2021-03-07 08:40:22 +0000

    added some count data and page cleaning up

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,7 +2,7 @@ class AppointmentsController < ApplicationController
   before_action :set_appointment, only: [:edit, :update, :destroy]
 
   def index
-    @appointments = Appointment.all.order("date DESC")
+    @appointments = Appointment.where("appointments.date >= ?", Time.now).order("date DESC")
   end
 
   def new
