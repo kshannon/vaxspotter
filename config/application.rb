fd666ae2ad26ccfc431367b1e1dc2da88fdbae63
Kyle Shannon <kyle@kmshannon.com>
2021-03-08 03:06:14 +0000

    update timezone

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,8 @@ module Vaxspotter
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Pacific Time (US & Canada)"
+    config.active_record.default_timezone = :utc # Or :local
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
