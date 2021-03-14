913396539cb0a8efee9a064fd9023fe8e8babe78
Kyle Shannon <kyle@kmshannon.com>
2021-03-14 10:20:42 +0000

    libraries to help with parsing json api

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative "boot"
 
 require "rails/all"
 require 'csv'
+require 'json'
+require 'net/http'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
