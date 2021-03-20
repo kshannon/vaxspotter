382e28fd8b12b0a7e18e6291817531dbb6684bee
Kyle Shannon <kyle@kmshannon.com>
2021-03-20 17:32:42 +0000

    add filtering to feed page with stimulus js

---
+++ app/javascript/controllers/filter_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  initialize() {
+    console.log('Hello from stimulus!');
+  }
+  static targets = [ "source", "filterable" ]
+  filter(event) {
+    let lowerCaseFilterTerm = this.sourceTarget.value.toLowerCase()
+
+    this.filterableTargets.forEach((el, i) => {
+      let filterableKey =  el.getAttribute("data-filter-key")
+
+      el.classList.toggle("hidden", !filterableKey.includes( lowerCaseFilterTerm ) )
+    })
+  }
+}
